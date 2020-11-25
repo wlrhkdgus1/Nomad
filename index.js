d7d0812de217a939d4477b85fe5463ffe8859c20
Ji Gwang Hyun <gwgw4578@gmail.com>
2020-11-26 03:50:35 +0900

    1126 nomad study

--- index.js
+++
@@ -1,1 +0,0 @@
-console.log("Im Working. Im JS. Im Beautiful.Im worth it")
---
+++ index01.js
@@ -0,0 +1,13 @@
+function sayHello(name,age){
+    return `Hello ${name} you are ${age} years old`;
+}
+    const g = sayHello('amarans',27);
+        console.log(g);
+        
+const calculator = {
+    plus: function(a, b){
+        return a * b;
+    }
+}
+const plus = calculator.plus(5, 5)
+console.log(plus);
