4fae21e085dc80dad3392d6ffebc8f997427f537
Ji Gwang Hyun <gwgw4578@gmail.com>
2020-12-10 16:20:34 +0900

    1210 watch

--- todo.js
+++ todo.js
@@ -27,7 +27,7 @@ function paintToDo(text){
   const delBtn = document.createElement("button");
   const span = document.createElement("span");
   const newId = toDos.length + 1;
-  delBtn.innerText = "❌";
+  delBtn.innerText = "🚮";
   delBtn.addEventListener("click", deleteToDo);
   span.innerText = text;
   li.appendChild(delBtn);
